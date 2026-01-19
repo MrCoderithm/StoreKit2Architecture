4e11cc7bd4ed091ecd3a2e4c22981a06a47ebd7d
Ali <theysee@live.ca>
2026-01-18 19:10:49 -0500

    Initial Commit

---
+++ StoreKit2Architecture/StoreKit2ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit2ArchitectureApp.swift
+//  StoreKit2Architecture
+//
+//  Created by Ali Muhammad on 2026-01-18.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit2ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoreKit2ArchitectureTests/StoreKit2ArchitectureTests.swift
@@ -0,0 +1,16 @@
+//
+//  StoreKit2ArchitectureTests.swift
+//  StoreKit2ArchitectureTests
+//
+//  Created by Ali Muhammad on 2026-01-18.
+//
+
+import Testing
+
+struct StoreKit2ArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
